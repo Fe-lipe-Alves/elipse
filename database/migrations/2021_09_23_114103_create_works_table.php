73fab0169f611809efcccb485f4e46657aadbc7d
Felipe Alves <felipealves1550@gmail.com>
2021-09-24 21:04:04 -0300

    Criado migração da tabela de trabalhos

---
+++ database/migrations/2021_09_23_114103_create_works_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('class_id')->constrained('classes');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('deadline');
+            $table->decimal('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
