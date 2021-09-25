9cba276b07931594d9b54f78997d08dcaae224b3
Felipe Alves <felipealves1550@gmail.com>
2021-09-25 19:22:50 -0300

    Criando funções de interação js para o formulário das aulas

---
+++ public/js/lesson.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/lesson.js ***!
+  \********************************/
+$(document).ready(function () {
+  $('.class_schedule-item').on('click', function () {
+    var selected = $(this).data('selected');
+
+    if (selected) {
+      $(this).removeClass('bg-indigo-300 hover:bg-indigo-200').addClass('hover:bg-indigo-100').data('selected', !selected).find('input').prop('checked', !selected);
+    } else {
+      $(this).addClass('bg-indigo-300 hover:bg-indigo-200').removeClass('hover:bg-indigo-100').data('selected', !selected).find('input').prop('checked', !selected);
+    }
+  });
+});
+/******/ })()
+;
