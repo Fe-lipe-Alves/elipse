b133af64c24cf678140d62c0de1ac2913013c6ee
Felipe Alves <felipealves1550@gmail.com>
2021-09-25 19:01:49 -0300

    Criando migração da tabela de horários das aulas

---
+++ app/Models/ClassSchedule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassSchedule extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'lesson_id',
+        'value',
+    ];
+
+    /**
+     * Obtém a aula relacionada ao horário
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+}
