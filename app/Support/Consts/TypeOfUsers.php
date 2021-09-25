db24522f5a8e786f61231a80a44322806fc5132d
Felipe Alves <felipealves1550@gmail.com>
2021-09-24 21:06:56 -0300

    Criando constantes de tipos de usuários

---
+++ app/Support/Consts/TypeOfUsers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Support\Consts;
+
+class TypeOfUsers
+{
+    public const ADMIN = 1;
+    public const STUDENT = 1;
+    public const TEACHER = 3;
+    public const SECRETARY = 3;
+}
